22517cdd0431fe1a8b3ffbc5a4f5c391099df81d
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-18 18:42:13 +0700

    Update tag_all.py

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -2,6 +2,7 @@ import re
 from userbot import CMD_HELP, bot
 from userbot.events import xubot_cmd
 from userbot import CUSTOM_CMD as xcm
+from telethon.tl.types import ChannelParticipantsAdmins
 
 usernexp = re.compile(r"@(\w{3,32})\[(.+?)\]")
 nameexp = re.compile(r"\[([\w\S]+)\]\(tg://user\?id=(\d+)\)\[(.+?)\]")
@@ -15,10 +16,40 @@ async def all(event):
     query = event.pattern_match.group(1)
     mentions = f"@all {query}"
     chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, 100000):
+    async for x in bot.iter_participants(chat, 1000):
         mentions += f"[\u2063](tg://user?id={x.id} {query})"
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
+@bot.on(xubot_cmd(outgoing=True, pattern="men(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    if event.reply_to_msg_id:
+        reply_msg = await event.get_reply_message()
+        u = reply_msg.sender_id
+    else:
+        user, input_str = input_str.split(" ", 1)
+        try:
+            u = int(user)
+        except ValueError:
+            try:
+                u = await event.client.get_entity(user)
+            except ValueError:
+                await event.delete()
+                return
+            u = int(u.id)
+        except Exception:
+            await event.delete()
+            return
+    await event.delete()
+    await event.client.send_message(
+        event.chat_id,
+        f"<a href='tg://user?id={u}'>{input_str}</a>",
+        parse_mode="HTML",
+        reply_to=reply_to_id,
+    )
 
 CMD_HELP.update({
     "tag_all":
