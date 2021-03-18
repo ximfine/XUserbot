a7f3c99a9fe9abc2599f1ed1abb363de382d98d2
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-19 00:56:39 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -11,6 +11,12 @@ import urllib.request
 from os import remove
 
 from PIL import Image
+import random
+import requests
+from asyncio.exceptions import TimeoutError
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.types import (
     DocumentAttributeFilename,
