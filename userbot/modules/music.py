5257c3783987bb420a169b1b3efcde0394d8d719
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-29 10:17:51 +0700

    Update music.py

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -16,7 +16,27 @@ from selenium import webdriver
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from validators.url import url
+from html import unescape
+from urllib.error import HTTPError
+import bs4
+from bs4 import BeautifulSoup
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (DownloadError, ContentTooShortError,
 
+                              ExtractorError, GeoRestrictedError,
+                              MaxDownloadsReached, PostProcessingError,
+                              UnavailableVideoError, XAttrMetadataError)
+
+try:
+
+   from youtubesearchpython import SearchVideos 
+
+except:
+	os.system("pip install pip install youtube-search-python")
+	from youtubesearchpython import SearchVideos 
+	pass
 from userbot import (
     CMD_HELP,
     DEEZER_ARL_TOKEN,
@@ -92,6 +112,129 @@ async def _(event):
         return await event.edit(
             "`Error: `@WooMaiBot` is not responding or Song not found!.`"
         )
+
+@bot.on(xubot_cmd(outgoing=True, pattern=r"song(?: |$)(.*)"))
+async def download_video(v_url):
+    lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
+    if not sender.id == me.id:
+        rkp = await lazy.edit("`processing...`")
+    else:
+    	rkp = await lazy.edit("`processing...`")   
+    url = v_url.pattern_match.group(1)
+    if not url:
+         return await rkp.edit("`Error \nusage song <song name>`")
+    search = SearchVideos(url, offset = 1, mode = "json", max_results = 1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get('search_result')
+    try:
+       url = q[0]['link']
+    except:
+    	return await rkp.edit("`failed to find`")
+    type = "audio"
+    await rkp.edit("`Preparing to download...`")
+    if type == "audio":
+        opts = {
+            'format':
+            'bestaudio',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'writethumbnail':
+            True,
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+            'outtmpl':
+            '%(id)s.mp3',
+            'quiet':
+            True,
+            'logtostderr':
+            False
+        }
+        video = False
+        song = True    
+    try:
+        await rkp.edit("`Fetching data, please wait..`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await rkp.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await rkp.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await rkp.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await rkp.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await rkp.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await rkp.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        await rkp.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await rkp.edit("`There was an error during info extraction.`")
+        return
+    except Exception as e:
+        await rkp.edit(f"{str(type(e)): {str(e)}}")
+        return
+    c_time = time.time()
+    if song:
+        await rkp.edit(f"`Preparing to upload song:`\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp3",
+            supports_streaming=True,
+            attributes=[
+                DocumentAttributeAudio(duration=int(rip_data['duration']),
+                                       title=str(rip_data['title']),
+                                       performer=str(rip_data['uploader']))
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp3")))
+        os.remove(f"{rip_data['id']}.mp3")
+        await rkp.delete()
+    elif video:
+        await rkp.edit(f"`Prosess upload song :`\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp4",
+            supports_streaming=True,
+            caption=url,
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp4")))
+        os.remove(f"{rip_data['id']}.mp4")
+        await rkp.delete()
+        os.system("rm *.mkv *.mp4 *.webm")
+    except BaseException:
+        os.system("rm *.mkv *.mp4 *.webm")
+        return
 
 
 @bot.on(xubot_cmd(outgoing=True, pattern=r"vsong(?: |$)(.*)"))
@@ -340,6 +483,8 @@ CMD_HELP.update(
         "\nUsage: Download music with @WooMaiBot"
         f"\n\n>`{xcm}netease now`"
         "\nUsage: Download current LastFM scrobble use `@WooMaiBot`."
+        f"\n\n>`{xcm}song` **Artist - Song Title**"
+        "\nUsage: Finding and uploading song by youtube."
         f"\n\n>`{xcm}vsong` **Artist - Song Title**"
         "\nUsage: Finding and uploading videoclip."
         f"\n\n>`{xcm}smd <Artist - Song Title>`"
