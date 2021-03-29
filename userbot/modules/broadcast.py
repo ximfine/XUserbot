fac68a4b2a65e614e9cc412e1bf784e996dd0d83
XIMFINE <xbotprojects@gmail.com>
2021-03-29 16:08:46 +0000

    pylint: auto fixes
    Signed-off-by: XIMFINE <xbotprojects@gmail.com>

--- userbot/modules/broadcast.py
+++ userbot/modules/broadcast.py
@@ -9,6 +9,7 @@ from userbot.modules.sql_helper import broadcast_sql as sql
 from userbot.events import xubot_cmd
 from userbot import CUSTOM_CMD as xcm
 
+
 @bot.on(xubot_cmd(outgoing=True, pattern=r"sendto ?(.*)"))
 async def catbroadcast_send(event):
     if event.fwd_from:
@@ -313,23 +314,19 @@ async def catbroadcast_delete(event):
         )
 
 
-
-CMD_HELP.update(
-    {
-        "broadcast": f">`{xcm}sendto <category_name> `"
-        "\n•Usage: __will send the replied message to all the chats in give category.__"
-        f"\n\n>`{xcm}fwdto <category_name>`"
-        "\n•Usage: __will forward the replied message to all the chats in give category.__"
-        f"\n\n>`{xcm}addto <category_name>`"
-        "\n•Usage: __It will add this chat/user/channel to the category of the given name.__"
-        f"\n\n>`{xcm}rmfrom <category name>`"
-        "\n•Usage: __To remove the Chat/user/channel from the given category name.__"
-        f"\n\n>`{xcm}list <category_name>`"
-        "\n•Usage: __Will show the list of all chats in the given category.__"
-        f"\n\n>`{xcm}listall`"
-        "\n•Usage: __Will show the list of all category names.__"
-        f"\n\n>`{xcm}frmfrom <category_name chat_id>`"
-        "\nUsage: __Mutes the person in all groups you have in common with them.__"
-        f"\n\n>`{xcm}delc <category_name>`"
-        "\nUsage: __Deletes the category completely in database__"
-        })
+CMD_HELP.update({"broadcast": f">`{xcm}sendto <category_name> `"
+                 "\n•Usage: __will send the replied message to all the chats in give category.__"
+                 f"\n\n>`{xcm}fwdto <category_name>`"
+                 "\n•Usage: __will forward the replied message to all the chats in give category.__"
+                 f"\n\n>`{xcm}addto <category_name>`"
+                 "\n•Usage: __It will add this chat/user/channel to the category of the given name.__"
+                 f"\n\n>`{xcm}rmfrom <category name>`"
+                 "\n•Usage: __To remove the Chat/user/channel from the given category name.__"
+                 f"\n\n>`{xcm}list <category_name>`"
+                 "\n•Usage: __Will show the list of all chats in the given category.__"
+                 f"\n\n>`{xcm}listall`"
+                 "\n•Usage: __Will show the list of all category names.__"
+                 f"\n\n>`{xcm}frmfrom <category_name chat_id>`"
+                 "\nUsage: __Mutes the person in all groups you have in common with them.__"
+                 f"\n\n>`{xcm}delc <category_name>`"
+                 "\nUsage: __Deletes the category completely in database__"})
