2c3ac70c5407eca204949a7a0b486d5ccb25ded8
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-29 21:03:50 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -98,7 +98,7 @@ UPSTREAM_REPO_URL = (os.environ.get("UPSTREAM_REPO_URL")
                      or "https://github.com/ximfine/XUserbot")
 
 # UPSTREAM_REPO_URL branch, the default is master
-UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "sql-extended"
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "Beta"
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get(
