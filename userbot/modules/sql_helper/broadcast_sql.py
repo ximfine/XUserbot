ae68df2fc4f78c329b07d3d3f470bcc45db14181
XIMFINE <xbotprojects@gmail.com>
2021-03-29 14:47:33 +0000

    pylint: auto fixes
    Signed-off-by: XIMFINE <xbotprojects@gmail.com>

--- userbot/modules/sql_helper/broadcast_sql.py
+++ userbot/modules/sql_helper/broadcast_sql.py
@@ -15,7 +15,8 @@ class CatBroadcast(BASE):
         self.group_id = str(group_id)
 
     def __repr__(self):
-        return "<Cat Broadcast channels '%s' for %s>" % (self.group_id, self.keywoard)
+        return "<Cat Broadcast channels '%s' for %s>" % (
+            self.group_id, self.keywoard)
 
     def __eq__(self, other):
         return bool(
@@ -44,14 +45,17 @@ def add_to_broadcastlist(keywoard, group_id):
 
         SESSION.merge(broadcast_group)
         SESSION.commit()
-        BROADCAST_SQL_.BROADCAST_CHANNELS.setdefault(keywoard, set()).add(str(group_id))
+        BROADCAST_SQL_.BROADCAST_CHANNELS.setdefault(
+            keywoard, set()).add(str(group_id))
 
 
 def rm_from_broadcastlist(keywoard, group_id):
     with CATBROADCAST_INSERTION_LOCK:
-        broadcast_group = SESSION.query(CatBroadcast).get((keywoard, str(group_id)))
+        broadcast_group = SESSION.query(
+            CatBroadcast).get((keywoard, str(group_id)))
         if broadcast_group:
-            if str(group_id) in BROADCAST_SQL_.BROADCAST_CHANNELS.get(keywoard, set()):
+            if str(group_id) in BROADCAST_SQL_.BROADCAST_CHANNELS.get(
+                    keywoard, set()):
                 BROADCAST_SQL_.BROADCAST_CHANNELS.get(keywoard, set()).remove(
                     str(group_id)
                 )
@@ -66,7 +70,8 @@ def rm_from_broadcastlist(keywoard, group_id):
 
 def is_in_broadcastlist(keywoard, group_id):
     with CATBROADCAST_INSERTION_LOCK:
-        broadcast_group = SESSION.query(CatBroadcast).get((keywoard, str(group_id)))
+        broadcast_group = SESSION.query(
+            CatBroadcast).get((keywoard, str(group_id)))
         return bool(broadcast_group)
 
 
@@ -113,7 +118,10 @@ def num_broadcastlist_chat(keywoard):
 
 def num_broadcastlist_chats():
     try:
-        return SESSION.query(func.count(distinct(CatBroadcast.keywoard))).scalar()
+        return SESSION.query(
+            func.count(
+                distinct(
+                    CatBroadcast.keywoard))).scalar()
     finally:
         SESSION.close()
 
