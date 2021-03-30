358594d7d1c48f8a9b291d927881b55d926f686f
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-30 09:48:43 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -65,3 +65,48 @@ async def _(event):
         else:
             await event.edit(asu.message)
             await event.client.delete_messages(conv.chat_id, [jemboed.id, asu.id])
+
+
+@bot.on(xubot_cmd(outgoing=True, pattern="skey(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    query = event.pattern_match.group(1)
+    await event.edit(f"```Checking SK KEY {query}```")
+    async with bot.conversation("@Carol5_bot") as conv:
+        try:
+            jemboed = await conv.send_message(f"/bin {query}")
+            await asyncio.sleep(5)
+            asu = await conv.get_response()
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            return await event.reply("Unblock @Carol5_bot atau chat dulu")
+        if asu.text.startswith("Wait for result..."):
+            return await event.edit("SK KEY Invalid!")
+        else:
+            await event.edit(asu.message)
+            await event.client.delete_messages(conv.chat_id, [jemboed.id, asu.id])
+
+
+@bot.on(xubot_cmd(outgoing=True, pattern="nmap(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@scriptkiddies_bot"  # pylint:disable=E0602
+    nmap = f"nmap"  # pylint:disable=E0602
+    await event.edit("Processing....")
+    async with bot.conversation("@scriptkiddies_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=510263282))
+            await conv.send_message(f'/{nmap} {link}')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("Unblock @ scriptkiddies_bot dulu Goblok!!")
+            return
+        else:
+            await event.edit(f"{response.message.message}")
+            await event.client.delete_messages(conv.chat_id)
