7298e393684d83d5f9b6b034a1ef160f8d9cab93
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-30 08:06:58 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -23,8 +23,8 @@ async def _(event):
             await event.reply("```Unblock plox```")
             return
         else:
-            await event.sleep(9)
             await event.edit(f"{response.message.message}")
+            await bot.forward_messages(event.chat_id, response.message)            
             await event.client.delete_messages(conv.chat_id, [response.id, link1.id])
 
 
