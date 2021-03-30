67bc43eca613f7381af21678169ee2c7a33f3b61
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-30 08:00:00 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -20,13 +20,14 @@ async def _(event):
                     from_users=1247032902))
             link1 = await conv.send_message(f'/{gen} {link}')
             response = await response
+            response2 = await response
             await asyncio.sleep(8)
         except YouBlockedUserError:
             await event.reply("```Unblock plox```")
             return
         else:
             await event.edit(f"{response.message.message}")
-            await event.edit(f"{response.message.message}")
+            await event.edit(f"{response2.message.message}")
             await event.client.delete_messages(conv.chat_id, [response.id, link1.id])
 
 
