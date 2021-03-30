c223e41f5b5cebfe864fda0fb6e2565ce9590679
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-30 09:39:05 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -9,11 +9,11 @@ async def _(event):
     if event.fwd_from:
         return
     query = event.pattern_match.group(1)
-    await event.edit("```Processing```")
+    await event.edit(f"```Generated CC {query}..```")
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             jemboed = await conv.send_message(f"/gen {query}")
-            await asyncio.sleep(8)
+            await asyncio.sleep(5)
             asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
@@ -30,11 +30,11 @@ async def _(event):
     if event.fwd_from:
         return
     query = event.pattern_match.group(1)
-    await event.edit("```Processing```")
+    await event.edit("```Checking CC Number..```")
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             jemboed = await conv.send_message(f"/ss {query}")
-            await asyncio.sleep(8)
+            await asyncio.sleep(6)
             asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
@@ -51,11 +51,11 @@ async def _(event):
     if event.fwd_from:
         return
     query = event.pattern_match.group(1)
-    await event.edit("```Processing```")
+    await event.edit(f"```Checking BIN {query}```")
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             jemboed = await conv.send_message(f"/bin {query}")
-            await asyncio.sleep(8)
+            await asyncio.sleep(6)
             asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
