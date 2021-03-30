8f9150f5e8cb02f7408348c88e8874dbf37198f3
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-30 09:34:02 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -21,5 +21,46 @@ async def _(event):
         if asu.text.startswith("Wait for result..."):
             return await event.edit(f"Gagal generate {query}!")
         else:
-            await event.client.send_message(event.chat_id, asu.message, caption="Generate Berhasil",)
+            await event.edit(asu.message)
+            await event.client.delete_messages(conv.chat_id, [jemboed.id, asu.id])
+
+
+@bot.on(xubot_cmd(outgoing=True, pattern="chk(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    query = event.pattern_match.group(1)
+    await event.edit("```Processing```")
+    async with bot.conversation("@Carol5_bot") as conv:
+        try:
+            jemboed = await conv.send_message(f"/ss {query}")
+            await asyncio.sleep(8)
+            asu = await conv.get_response()
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            return await event.reply("Unblock @Carol5_bot atau chat dulu")
+        if asu.text.startswith("Wait for result..."):
+            return await event.edit(f"Gagal Mengecek {query}!")
+        else:
+            await event.edit(asu.message)
+            await event.client.delete_messages(conv.chat_id, [jemboed.id, asu.id])
+
+@bot.on(xubot_cmd(outgoing=True, pattern="bin(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    query = event.pattern_match.group(1)
+    await event.edit("```Processing```")
+    async with bot.conversation("@Carol5_bot") as conv:
+        try:
+            jemboed = await conv.send_message(f"/bin {query}")
+            await asyncio.sleep(8)
+            asu = await conv.get_response()
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            return await event.reply("Unblock @Carol5_bot atau chat dulu")
+        if asu.text.startswith("Wait for result..."):
+            return await event.edit(f"Bin {query} Invalid!")
+        else:
+            await event.edit(asu.message)
             await event.client.delete_messages(conv.chat_id, [jemboed.id, asu.id])
