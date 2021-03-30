1cb59dac9872199c6b1af1253abf4398a39a31d4
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-30 09:06:48 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -12,12 +12,12 @@ async def _(event):
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             await conv.send_message(f"/gen {query}")
-            r1 = await conv.get_response()
+            asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             return await event.reply("Unblock @SaitamaRobot plox")
         if r1.text.startswith("Waiting"):
             return await event.edit(f"`No result found for` **{query}**")
         else:
-            await event.edit(r2.message)
+            await event.edit(asu.message)
             await event.client.delete_messages(conv.chat_id, [r1.id])
