161ad0ee136a9e2c3378501502e8e329632937d8
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-01 21:58:52 +0700

    Update json.py

--- userbot/modules/json.py
+++ userbot/modules/json.py
@@ -41,4 +41,4 @@ async def _(event):
         return
     catevent = await event.get_reply_message() if event.reply_to_msg_id else event
     the_real_message = catevent.stringify()
-    await event.edit(the_real_message, parse_mode=parse_pre)
+    await event.edit(the_real_message)
