4a51839cee17d72c96a43897393ab632a24dc7c7
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-02 09:23:37 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -40,7 +40,7 @@ DEF_UNAPPROVED_MSG = (
     f" {DEFAULTUSER} Menerima Pesanmu\n"
     "─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇\n\n"
     f"- ©Bot Assisten {DEFAULTUSER}\n"
-    "- Protected By 🔥XBOT-REMIX🔥")
+    "- Protected By 🔥 XUSERBOT 🔥")
 # =================================================================
 
 logo = ALIVE_LOGO
