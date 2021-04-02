153c90f90acab73f0b0e021bc3a525718e0a547c
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-02 07:57:30 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -85,9 +85,9 @@ async def permitpm(event):
                         event.chat_id, from_user="me", search=UNAPPROVED_MSG
                     ):
                         await message.delete()
-                    await event.reply.photo(f"{ALIVE_LOGO}, {UNAPPROVED_MSG}")
+                    await bot.send_file(f"event.chat_id, {ALIVE_LOGO}, caption={UNAPPROVED_MSG}")
             else:
-                await event.reply_photo(f"{ALIVE_LOGO}, {UNAPPROVED_MSG}")
+                await bot.send_file(f"event.chat_id, {ALIVE_LOGO}, caption={UNAPPROVED_MSG}")
             LASTMSG.update({event.chat_id: event.text})
             if notifsoff:
                 await event.client.send_read_acknowledge(event.chat_id)
